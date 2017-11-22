7888a4e50fa72c30293513b0b0338af0746c8af0
Ziamor <ziamor23@gmail.com>
2017-11-21 20:55:18 -0500

    Clean up of unused method

--- components/src/com/ziamor/incadium/components/Render/TerrainTileComponent.java
+++ components/src/com/ziamor/incadium/components/Render/TerrainTileComponent.java
@@ -8,11 +8,6 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 public class TerrainTileComponent extends Component {
     public TextureRegion region;
 
-    //TODO load textures correctly, take memory allocation and deallocation into consideration
-    public void setRegion(String fileName, int x, int y) {
-        region = new TextureRegion(new Texture(fileName), x * 32, y * 32, 32, 32);
-    }
-
     public void setRegion(Texture tex, int x, int y) {
         region = new TextureRegion(tex, x * 32, y * 32, 32, 32);
     }
